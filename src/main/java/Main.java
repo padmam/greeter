5861b96f5de152b3e4ccf6eee1ecadcbdbf8896d
Padma Mullagiri <mullagiri.padma@gmail.com>
2016-03-04 11:17:13 +0530

    made changes to artifact name in pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,5 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
-        get("/hello", (req, res) -> "Hi all");
     }
 }
