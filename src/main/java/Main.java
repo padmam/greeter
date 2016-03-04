e227eba817bb4ace617ae7da7af279c947dee5ba
Padma Mullagiri <mullagiri.padma@gmail.com>
2016-03-04 11:08:22 +0530

    changes to Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
-        get("/hi", (req, res) -> "Hi all");
+        get("/hello", (req, res) -> "Hi all");
     }
 }
